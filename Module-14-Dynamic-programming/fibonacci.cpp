17a643a46b5302b50d6035a8c3fbe80c3c375591
Arfaz31 <arfazahamed31@gmail.com>
2025-09-09 23:35:15 +0600

    Module-14-Dynamic-programming

---
+++ Module-14-Dynamic-programming/fibonacci.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int fibo(int n) // O(2^n)
+{
+    if (n < 2)
+        return n;
+    return fibo(n - 1) + fibo(n - 2);
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    cout << fibo(n);
+
+    return 0;
+}
